73f1986c6f8a5dcf5d2689afdb6194f1538011c1
nadirlaskar <nadirlaskar@gmail.com>
2022-11-21 21:03:36 +0530

    added edit profile

--- components/Authenticate.js
+++ components/Authenticate.js
@@ -1,13 +1,18 @@
 import classNames from 'classnames';
+import { useCallback, useState } from 'react';
 import { useSigninCheck, useUser } from 'reactfire';
 import useComponentWithFirebase from '../hooks/useComponentWithFirebase';
 import LogoutUser from '../libs/logoutUser.js';
 import SignInWithGoogle from '../libs/signInWithGoogle';
+import Modal from './ui-blocks/popup';
+
 const UserInfo = ({
   showImage = true, showHandle = true, showName = true, showTweetCount = false,
-  imageClassNames = [],
-  metaInfoStyles = [],
-  rootStyles = []
+  imageClassNames,
+  metaInfoStyles ,
+  rootStyles,
+  handleStyles,
+  nameStyles,
 }) => { 
   const { status, data: user } = useUser();
 
@@ -16,14 +21,14 @@ const UserInfo = ({
   }
   
   return (
-    <div className={classNames('flex-row items-center inline-flex', ...rootStyles)}>
+    <div className={classNames('flex-row items-center inline-flex', rootStyles)}>
       {showImage && (
         <img width={40} height={40}
           className={
             classNames(
               'rounded-full', 'inline-block',
               { "mr-2 my-4": showHandle || showName || showTweetCount },
-              ...imageClassNames
+              imageClassNames
             )
           }
           src={user.photoURL}
@@ -31,14 +36,62 @@ const UserInfo = ({
         />
       )
       }
-      <div className={classNames('inline-block', ...metaInfoStyles)}>
-        {showName && <div className='leading-3'>{user.displayName}</div>}
-        {showHandle && <div className='text-sm text-slate-500'>@{user.email.replace(/@.+/g, '')}</div>}
+      <div className={classNames('inline-block', metaInfoStyles)}>
+        {showName && <div className={classNames('leading-3', nameStyles)}>{user.displayName}</div>}
+        {showHandle && <div className={classNames('text-sm text-slate-500',handleStyles)}>@{user.email.replace(/@.+/g, '')}</div>}
         {showTweetCount && <div className='text-sm text-slate-500 pt-2'>0 Tweets</div>}
       </div>
     </div>
   )
 }
+
+export const ShowUserInfo = (props) => { 
+  return useComponentWithFirebase('auth', UserInfo, props)
+}
+
+export const UserInfoWithCoverPic = ({isEdit}) => { 
+  return (
+    <>
+      <img src='https://pbs.twimg.com/profile_banners/714176889460432900/1570041174/1500x500' className='w-full h-full' />
+      <div className={classNames(
+        'relative',
+        {
+          'bottom-20 left-4': !isEdit,
+          'bottom-14 left-4': isEdit
+        },
+        'inline-block'
+      )}>
+        <ShowUserInfo
+          showImage={true}
+          showHandle={!isEdit}
+          showName={!isEdit}
+          imageClassNames={classNames(
+            {
+              'w-32': !isEdit,
+              'w-28': isEdit,
+            },
+            'h-auto border-4 border-white rounded-full'
+          )}
+          metaInfoStyles={classNames(
+            {
+              'text-xl': !isEdit,
+              'text-sm': isEdit
+            },
+            'text-black', 'block'
+          )}
+          rootStyles={'flex flex-col items-start'}
+          nameStyles={classNames(
+            'font-semibold', {
+              'leading-6': !isEdit,
+              'leading-2': isEdit
+          }
+          )}
+        />
+      </div>
+    </>
+  )
+}
+
 function Authenticate() { 
    const { status, data: signInCheckResult } = useSigninCheck();
 
@@ -49,10 +102,10 @@ function Authenticate() {
   if (signInCheckResult.signedIn === true) {
     return <>
       <UserInfo />
-      <button className='text-white bg-slate-400 p-2 rounded-full hover:bg-sky-400 text-sm w-full' onClick={LogoutUser}>Logout</button>
+      <button className='text-white bg-slate-400 p-4 rounded-full hover:bg-sky-600 text-sm w-full' onClick={LogoutUser}>Logout</button>
     </>;
   } else {
-     return <button  className='text-white bg-sky-500 p-2 rounded-full hover:bg-sky-600 text-lg w-full'  onClick={() => { SignInWithGoogle(); }}>Sign In</button>
+     return <button  className='text-white bg-sky-500 p-4 rounded-full hover:bg-sky-600 text-lg w-full'  onClick={() => { SignInWithGoogle(); }}>Sign In</button>
   }
 }
 
@@ -60,6 +113,29 @@ export default  function AuthenticateWithFirebase () {
   return useComponentWithFirebase('auth', Authenticate, {})
 }
 
-export const ShowUserInfo = (props) => { 
-  return useComponentWithFirebase('auth', UserInfo, props)
+function EditProfile({className}) {
+  const [openEditModal, setOpenEditModal] = useState(false);
+  const toggleEditProfileModal = useCallback(() => { 
+    setOpenEditModal(!openEditModal);
+  }, [openEditModal, setOpenEditModal])
+  return(
+    <>
+      <button className={
+        classNames(
+          'inline-block py-2 px-4 rounded-full border hover:bg-slate-100 text-sm font-semibold',
+          className
+        )}
+        onClick={toggleEditProfileModal}
+      >
+        Edit Profile
+      </button>
+      <Modal isOpen={openEditModal} onClose={toggleEditProfileModal} title={'Edit Profile'}>
+        <UserInfoWithCoverPic isEdit={true} />
+      </Modal>
+    </>
+  )
+}
+
+export const EditProfileButton = (props) => { 
+  return useComponentWithFirebase('auth', EditProfile, props)
 }
--- components/Sidebar.js
+++ components/Sidebar.js
@@ -2,38 +2,38 @@ import { HomeIcon, UserCircleIcon, UserGroupIcon, UserPlusIcon } from '@heroicon
 import classNames from 'classnames';
 import Link from 'next/link';
 import Authenticate from '../components/Authenticate';
-const listItemStyle = `p-4 w-full flex items-center border hover:rounded-full border-transparent hover:bg-slate-100 cursor-pointer relative`;
+const listItemStyle = `p-4 w-full flex items-center border hover:rounded-full border-transparent cursor-pointer relative`;
 const activeStyle = `absolute left-9 top-3 w-2 h-2 rounded-full bg-sky-500 text-white text-center`;
 const Sidebar = ({page}) => {
   return (
     <div className='flex flex-col h-full relative'>
       <ul className='my-4 h-full min-w-max'>
         <Link href='/'>
-          <li className={listItemStyle}>
+          <li className={classNames(listItemStyle,'hover:bg-slate-100')}>
             <HomeIcon className='inline-block mr-4' width={24} />
             {page === 'home' && <span className={activeStyle}/>}
             <span className={classNames(`text-xl`, { 'font-semibold': page === 'home' })}>Home</span>
           </li>
         </Link>
         <Link href='/me'>
-          <li className={listItemStyle}>
+          <li className={classNames(listItemStyle,'hover:bg-slate-100')}>
             <UserCircleIcon className='inline-block mr-4' width={24} />
             {page === 'profile' && <span className={activeStyle}/>}
             <span className={classNames(`text-xl`, { 'font-semibold': page === 'profile' })}>Profile</span>
           </li>
         </Link>
-        <li className={listItemStyle}>
+        <li className={classNames(listItemStyle,'hover:bg-slate-100')}>
           <UserPlusIcon className='inline-block mr-4' width={24} />
           {page === 'following' && <span className={activeStyle}/>}
           <span className={classNames(`text-xl`, { 'font-semibold': page === 'following' })}>Following</span>
         </li>
-        <li className={listItemStyle}>
+        <li className={classNames(listItemStyle,'hover:bg-slate-100')}>
           <UserGroupIcon className='inline-block mr-4' width={24} />
           {page === 'follower' && <span className={activeStyle}/>}
           <span className={classNames(`text-xl`, { 'font-semibold': page === 'follower' })}>Follower</span>
         </li>
-        <li className={listItemStyle + ' bg-sky-500 rounded-full text-white w-full p-2 hover:bg-sky-600 justify-center mt-5'}>
-          <span className='text-lg'>Tweet</span>
+        <li className={classNames(listItemStyle, 'py-2 bg-sky-500 rounded-full text-white w-full  hover:bg-sky-600 justify-center mt-5')}>
+          <span className='text-lg font-semibold'>Tweet</span>
         </li>
       </ul>
       <div className='absolute bottom-20 left-0 right-0'>
