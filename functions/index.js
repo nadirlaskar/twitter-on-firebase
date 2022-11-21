68d08b32dcaa763c3f1cd433f2e2e426aa13048d
nadirlaskar <nadirlaskar@gmail.com>
2022-11-22 00:30:30 +0530

    added createUser function

--- functions/index.js
+++ functions/index.js
@@ -1,9 +1,27 @@
 const functions = require("firebase-functions");
+const admin = require("firebase-admin");
+admin.initializeApp();
 
-// Create and Deploy Your First Cloud Functions
-// https://firebase.google.com/docs/functions/write-firebase-functions
+const addUserToFireStore = (user) => {
+  return admin.firestore()
+      .collection("users")
+      .doc(user.uid)
+      .create({
+        name: user.displayName,
+        email: user.email,
+        role: "user",
+        isActive: true,
+      });
+};
 
-exports.helloWorld = functions.https.onRequest((request, response) => {
-  functions.logger.info("Hello logs!", {structuredData: true});
-  response.send("Hello from Firebase!");
-});
+const deleteUserFromFireStore = (user) => {
+  return admin.firestore()
+      .collection("users")
+      .doc(user.uid)
+      .delete();
+};
+
+exports.createUserListener = functions.auth.user().onCreate(addUserToFireStore);
+exports.deleteUserListener = functions.auth
+    .user()
+    .onDelete(deleteUserFromFireStore);
