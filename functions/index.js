c66c57b5b12c84c3df8e31ee054c346be6f4dd4e
nadirlaskar <nadirlaskar@gmail.com>
2022-11-23 16:19:10 +0530

    added follower following functions

--- functions/index.js
+++ functions/index.js
@@ -48,3 +48,68 @@ exports.updateProfile = functions.https.onCall(async (data, context) => {
   await userRef.update({name, bio, dob, location, profession});
   return {message: "Profile updated successfully"};
 });
+
+const fetchUserFromFireStoreByHandle = async (handle) => {
+  const userRef = admin.firestore().collection("users");
+  const q = userRef.where("handle", "==", handle);
+  const querySnapshot = await q.get();
+  if (querySnapshot.empty) {
+    throw new functions.https.HttpsError("not-found", "User not found");
+  }
+  const ref = querySnapshot.docs[0].ref;
+  const data = ref.data();
+  data.id = querySnapshot.docs[0].id;
+  return {ref, data};
+};
+
+exports.followUser = functions.https.onCall(async (handle, context) => {
+  const uid = context.auth.uid;
+  const followerRef = admin.firestore().collection("users").doc(uid);
+  const follower = await followerRef.get();
+  if (!follower.exists) {
+    throw new functions.https.HttpsError("not-found", "User not found");
+  }
+  const {
+    ref: followingRef,
+    data: followingData,
+  } = await fetchUserFromFireStoreByHandle(handle);
+  const followingList = followingData.following;
+  if (followingList.includes(handle)) {
+    throw new functions
+        .https
+        .HttpsError("already-exists", "User already followed");
+  }
+  await followerRef.update({
+    following: admin.firestore.FieldValue.arrayUnion(handle),
+  });
+  await followingRef.update({
+    followers: admin.firestore.FieldValue.arrayUnion(follower.data().handle),
+  });
+  return {message: "User followed successfully"};
+});
+
+exports.unfollowUser = functions.https.onCall(async (handle, context) => {
+  const uid = context.auth.uid;
+  const followerRef = admin.firestore().collection("users").doc(uid);
+  const follower = await followerRef.get();
+  if (!follower.exists) {
+    throw new functions.https.HttpsError("not-found", "User not found");
+  }
+  const {
+    ref: followingRef,
+    data: followingData,
+  } = await fetchUserFromFireStoreByHandle(handle);
+  const followingList = followingData.following;
+  if (!followingList.includes(handle)) {
+    throw new functions
+        .https
+        .HttpsError("not-found", "User not followed");
+  }
+  await followerRef.update({
+    following: admin.firestore.FieldValue.arrayRemove(handle),
+  });
+  await followingRef.update({
+    followers: admin.firestore.FieldValue.arrayRemove(follower.data().handle),
+  });
+  return {message: "User unfollowed successfully"};
+});
