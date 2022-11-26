d6ffb97808e06938496b2a3fb267e0ba8c137d12
nadirlaskar <nadirlaskar@gmail.com>
2022-11-26 14:44:45 +0530

    fix tweet icon

--- components/Sidebar.js
+++ components/Sidebar.js
@@ -6,8 +6,8 @@ import { useSigninCheck } from 'reactfire';
 import Authenticate from '../components/Authenticate';
 import useComponentWithFirebase from '../hooks/useComponentWithFirebase';
 import TwitterHashtagIcon from './icons/HashtagIcon';
-const listItemStyle = `p-2 lg:p-4 w-full flex items-center border hover:rounded-full border-transparent cursor-pointer relative`;
-const activeStyle = `absolute left-9 top-3 w-2 h-2 rounded-full bg-sky-500 text-white text-center`;
+const listItemStyle = `p-2 lg:p-4 w-full flex items-center border hover:rounded-full border-transparent cursor-pointer`;
+const activeStyle = `absolute left-6 top-1 md:left-9 md:top-3 w-2 h-2 rounded-full bg-sky-500 text-white text-center`;
 const Sidebar = ({ page }) => {
   const { status, data: signInCheckResult } = useSigninCheck();
   return (
@@ -16,40 +16,41 @@ const Sidebar = ({ page }) => {
         { status === 'success' && (
           <>
             <Link href='/'>
-            <li className={classNames(listItemStyle, 'hover:bg-slate-100')}>
+            <li className={classNames(listItemStyle, 'hover:bg-slate-100 relative')}>
               <HomeIcon className='inline-block lg:mr-4 w-5 md:w-6'  />
               {page === 'home' && <span className={activeStyle} />}
               <span className={classNames(`text-xl hidden lg:inline`, { 'font-semibold': page === 'home' })}>Home</span>
             </li>
             </Link>
             <Link href='/u/search'>
-            <li className={classNames(listItemStyle, 'hover:bg-slate-100 lg:hidden')}>
+            <li className={classNames(listItemStyle, 'hover:bg-slate-100 lg:hidden relative')}>
               <MagnifyingGlassIcon className='inline-block lg:mr-4 w-5 md:w-6'  />
             </li>
             </Link>
-             <li className={classNames(listItemStyle, 'hover:bg-slate-100')}>
+             <li className={classNames(listItemStyle, 'hover:bg-slate-100 relative')}>
               <TwitterHashtagIcon className='inline-block lg:mr-4 w-5 md:w-6'  />
               {page === 'follower' && <span className={activeStyle} />}
               <span className={classNames(`text-xl hidden lg:inline`, { 'font-semibold': page === 'follower' })}>Explore</span>
             </li>
-            <li className={classNames(listItemStyle, 'hover:bg-slate-100', { 'hidden': !signInCheckResult.signedIn })}>
+            <li className={classNames(listItemStyle, 'hover:bg-slate-100 relative' , { 'hidden': !signInCheckResult.signedIn })}>
               <BellIcon className='inline-block lg:mr-4 w-5 md:w-6'  />
               {page === 'following' && <span className={activeStyle} />}
               <span className={classNames(`text-xl hidden lg:inline`, { 'font-semibold': page === 'following' })}>Notification</span>
             </li>
             <Link href='/me'>
-              <li className={classNames(listItemStyle, 'hover:bg-slate-100',{ 'hidden': !signInCheckResult.signedIn })}>
+              <li className={classNames(listItemStyle, 'hover:bg-slate-100 relative',{ 'hidden': !signInCheckResult.signedIn })}>
                 <UserCircleIcon className='inline-block lg:mr-4 w-5 md:w-6'  />
                 {page === 'profile' && <span className={activeStyle} />}
                 <span className={classNames(`text-xl hidden lg:inline`, { 'font-semibold': page === 'profile' })}>Profile</span>
               </li>
             </Link>
             <li className={classNames(
+              'fixed bottom-4 right-4 sm:static',
               listItemStyle, 'lg:p-3 bg-sky-500 rounded-full text-white lg:w-full lg:h-fit w-12 h-12 hover:bg-sky-600 mt-5',
               { 'lg:hidden': !signInCheckResult.signedIn }
             )}>
               <span className='w-full text-center text-lg font-semibold lg:inline-block hidden'>Tweet</span>
-              <PencilSquareIcon className='lg:hidden sm:block w-4 h-4 m-auto'/>
+              <PencilSquareIcon className='lg:hidden  w-4 h-4 m-auto'/>
             </li>
           </>
         )}
--- pages/index.js
+++ pages/index.js
@@ -10,9 +10,9 @@ export default function Home() {
         <title>Home</title>
         <link rel="icon" href="/favicon.ico" />
       </Head>
-      <main className='h-2/4'>
+      <main className='h-3/4'>
         <h1 className='text-xl mt-2 text-sky-500 font-semibold p-2 ml-4'>Home</h1>
-        <div className={classNames('flex flex-col justify-end items-center w-fit h-2/4 text-slate-500 text-sm m-12 md:m-auto text-center')}>
+        <div className={classNames('flex flex-col justify-end items-center w-fit h-2/4 text-slate-500 text-sm p-12 m-auto text-center')}>
          <XCircleIcon className='w-10 mb-2 text-slate-300'/>
           We’re sorry we don’t have tweets to show now.
         </div>
