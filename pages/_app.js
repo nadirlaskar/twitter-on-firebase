1898c3f2cbea20b5803ec41e76bd8518bed8f5c3
nadirlaskar <nadirlaskar@gmail.com>
2022-11-26 15:01:48 +0530

    added user scaleing

--- pages/_app.js
+++ pages/_app.js
@@ -1,3 +1,4 @@
+import Head from 'next/head';
 import { FirebaseAppProvider } from 'reactfire';
 import firebaseConfig from '../firebase';
 import '../styles/global.css';
@@ -5,6 +6,9 @@ import '../styles/global.css';
 export default function App({ Component, pageProps }) {
   return (
     <FirebaseAppProvider firebaseConfig={firebaseConfig}>
+      <Head>
+        <meta name="viewport" content="width=device-width, user-scalable=none" />
+      </Head>
         <Component {...pageProps} />
     </FirebaseAppProvider>
   );
